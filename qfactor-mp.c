67563daaddddcf20c257edae72f79bb18ee3ddc9
t-site <sktruntime@hotmail.co.jp>
2019-11-26 17:35:35 +0900

    some faster hack

--- qfactor-mp.c
+++ qfactor-mp.c
@@ -21,27 +21,30 @@ void qf2( mpz_t x  )
 	int facflag=1;
 	for( i = 2 ; i < THRESH ; i++ )
 	{
-		while( mpz_cdiv_r_ui(tmp ,out , i ) == 0 )
+		while( mpz_tdiv_r_ui(tmp ,out , i ) == 0 )
 		{
 			printf("%d ",i);
-			mpz_cdiv_q_ui(out,out,i);
+			mpz_tdiv_q_ui(out,out,i);
 			mpz_mul_ui(cx,cx,i);
 		}
 	}
 	mpz_set_ui(diff,THRESH);
+	mpz_init_set_ui(ff,THRESH);
 	mpz_init(r);
 	mpz_init(sr);
 	mpz_init(xhalf);
-	mpz_cdiv_q(xhalf,x,two);
+	mpz_tdiv_q(xhalf,x,two);
 	while( mpz_cmp(x,cx) != 0 )
 	{
 		mpz_urandomm(r,randomfp,diff);
-		mpz_cdiv_q(xhalf,diff,two);
+		mpz_tdiv_q(xhalf,diff,two);
 		mpz_sub(r,r,xhalf);
 		mpz_tdiv_r(sr,r,diff);
 		mpz_add(s,s,sr);
 		if( mpz_cmp(s,two) < 0 )
 			mpz_set(s,two);
+		else if( mpz_cmp(out,s) < 0 )
+			mpz_set( s,out);
 		
 		mpz_tdiv_r(diff,out,s);
 		facflag=mpz_cmp(diff,zero);
@@ -49,16 +52,17 @@ void qf2( mpz_t x  )
 		{
 			mpz_out_str(stdout,10,s);
 			puts("");
-			mpz_cdiv_q(out,out,s);
+			mpz_tdiv_q(out,out,s);
 			mpz_mul(cx,cx,s);
 			mpz_tdiv_r(diff,out,s);
 			facflag=mpz_cmp(diff,zero);
+			mpz_tdiv_q(ff,s,two);
 		}
 		mpz_sub(tmp,s,diff);
 		if ( mpz_cmp(tmp,diff) < 0 )
 			mpz_set(diff,tmp);
-		if ( mpz_cmp_ui(diff,THRESH ) < 0 )
-			mpz_set_ui(diff,THRESH);
+		if ( mpz_cmp(diff,ff ) < 0 )
+			mpz_set(diff,ff);
 
 	}
 }
